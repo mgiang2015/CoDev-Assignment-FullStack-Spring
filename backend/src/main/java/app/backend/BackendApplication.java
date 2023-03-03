fa2a467b62497bab948d0e4d4d003456ec337316
Le Minh Giang <mgiang2015@gmail.com>
2023-03-03 10:29:10 +0800

    got cors to work on backend

--- backend/src/main/java/app/backend/BackendApplication.java
+++ backend/src/main/java/app/backend/BackendApplication.java
@@ -2,9 +2,12 @@ package app.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @SpringBootApplication
 @RestController
@@ -14,6 +17,7 @@ public class BackendApplication {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@CrossOrigin
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,20 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const backendUrl = "http://localhost:8080"
+  const [message, setMessage] = useState("")
+
+  useEffect(() => {
+    fetch(backendUrl + "/greeting", { mode: 'cors' })
+      .then((res) => res.json())
+      .then((data) => setMessage(data.content));
+  }, []);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>{message}</h1>
     </div>
   )
 }
