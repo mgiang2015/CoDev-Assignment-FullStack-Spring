4c90672e436a933bbf874b7d2e8a8d414406192f
Le Minh Giang <mgiang2015@gmail.com>
2023-03-03 10:29:38 +0800

    remove unnecessary method

--- backend/src/main/java/app/backend/BackendApplication.java
+++ backend/src/main/java/app/backend/BackendApplication.java
@@ -2,12 +2,8 @@ package app.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.atomic.AtomicLong;
 
 @SpringBootApplication
 @RestController
@@ -15,11 +11,5 @@ public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
-	}
-
-	@CrossOrigin
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
 	}
 }
