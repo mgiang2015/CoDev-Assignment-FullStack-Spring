8ec5eca5e5a9dad0a2cc3b6efc91ff49748e05d8
Le Minh Giang <mgiang2015@gmail.com>
2023-03-05 15:12:20 +0800

    Book dropdown working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,28 +2,60 @@ import { useState, useEffect } from 'react'
 import BookDropdown from './components/BookDropdown'
 import './App.css'
 
-const top3Books = [
-  {
-    bookName: "Hi",
-    author: "Markiplier"
-  },{
-    bookName: "Hello World",
-    author: "C++"
-  },{
-    bookName: "Tale of Kittens",
-    author: "yes"
-  }
-]
-
 function App() {
   const backendUrl = "http://localhost:8080"
-  const [message, setMessage] = useState("HELLO EVERYONE")
+  const [top3Books, setTop3Books] = useState([]);
+
+  useEffect(() => {
+    setTop3Books([
+      {
+        name: "Hi",
+        author: "Markiplier",
+        isOpen: false,
+        borrowers: [
+          "Jesse Pinkman",
+          "Gustavo Fring",
+          "Walter White"
+        ]
+      },{
+        name: "Hello World",
+        author: "C++",
+        isOpen: false,
+        borrowers: [
+          "Jesse Pinkman",
+          "Gustavo Fring",
+          "Walter White"
+        ]
+      },{
+        name: "Tale of Kittens",
+        author: "yes",
+        isOpen: false,
+        borrowers: [
+          "Jesse Pinkman",
+          "Gustavo Fring",
+          "Walter White"
+        ]
+      }
+    ])
+  }, []);
+
+  const toggle = (bookIndex) => {
+    const newTop3 = top3Books.map((book, index) => {
+      if (index ===  bookIndex) {
+        return {...book, isOpen: !book.isOpen}
+      }
+
+      return {...book, isOpen: false};
+    })
+
+    setTop3Books(newTop3);
+  }
 
   return (
     <div className="App">
       <div id="container" className="bookContainer">
         {
-          top3Books.map((book, index) => <BookDropdown key={index} id={index + 1} bookName={book.bookName} author={book.author} />)
+          top3Books.map((book, index) => <BookDropdown key={index} id={index + 1} bookName={book.name} author={book.author} borrowers={book.borrowers} isOpen={book.isOpen} toggle={() => toggle(index)} />)
         }
       </div>
     </div>
--- frontend/src/components/BookDropdown.jsx
+++ frontend/src/components/BookDropdown.jsx
@@ -1,14 +1,24 @@
 import "./BookDropdown.css"
 
-export default function BookDropdown({id, bookName, author}) {
+export default function BookDropdown({id, bookName, author, borrowers, isOpen, toggle}) {
   return (
-  <div id={"book-item-" + id} className="BookItem">
-    <span className="id">{id}</span>
-    <span className="name">{bookName}</span>
-    <div className="toggleContainer">
-      <div className="book-toggle" />
+  <div className="bookAndChildrenContainer">
+    <div id={"book-item-" + id} className="BookItem">
+      <span className="id">{id}</span>
+      <span className="name">{bookName}</span>
+      <div className="toggleContainer">
+        <div className="book-toggle" onClick={toggle} />
+      </div>
+      <span className="author">{"by " + author}</span>
     </div>
-    <span className="author">{"by " + author}</span>
+    {
+      isOpen 
+      ?
+      borrowers.map(name => {
+        return <p className="customer">{name}</p>
+      })
+      : null
+    }
   </div>
   )
 }
